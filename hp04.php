909cda903f323e2992c4ed824797ef05900b4e06
davidzamber2 <davidzamber2@icloud.com>
2020-11-20 22:38:23 +0100

    Colaborador

---
+++ hp04.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>Harry Porlotes y El Malware de Fuego</title>
+ </head>
+ <body>
+<?php echo '<h1>Harry Porlotes y El Malware de Fuego</h1>'; ?>
+<?php print("<p>Un importante hackaton se celebra en la escuela y el portátil de un compañero de Harry, se avería debido a un ataque de Ransomwaremort, perdiendo así toda la saga Crepúsculo en epub que tenía guardada y las revistas de Marie-Claire de su abuela en PDF.</p><br><p>Es mucho más difícil juzgarse a sí mismo, que juzgar a los otros. Si consigues juzgarte rectamente, es que eres un verdadero sabio.</p>;"
+           );?>
+ </body>
+</html>
